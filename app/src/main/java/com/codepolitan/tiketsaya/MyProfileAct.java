1eb5b52e2bcc3dd79bfe637b48f00b55bbe61f4c
nurzain-pradana <pradananurzain@gmail.com>
2020-02-11 14:54:11 +0700

    finishing integrasi firebase myprofile, mulai integrasi firebase myticket

--- app/src/main/java/com/codepolitan/tiketsaya/MyProfileAct.java
+++ app/src/main/java/com/codepolitan/tiketsaya/MyProfileAct.java
@@ -1,24 +1,71 @@
 package com.codepolitan.tiketsaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MyProfileAct extends AppCompatActivity {
     LinearLayout item_my_ticket;
     Button btn_edit_profile;
     Button btn_sign_out;
 
+    TextView nama_lengkap, bio;
+    ImageView photo_profile;
+
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
+    String username_key_new = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
+        getUsernameLocal();
+
         item_my_ticket = findViewById(R.id.item_my_ticket);
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+
+        nama_lengkap = findViewById(R.id.nama_lengkap);
+        bio = findViewById(R.id.bio);
+        photo_profile = findViewById(R.id.photo_profile);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username_key_new);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                nama_lengkap.setText(dataSnapshot.child("nama_lengkap").getValue().toString());
+                bio.setText(dataSnapshot.child("bio").getValue().toString());
+
+                Picasso.with(MyProfileAct.this)
+                        .load(dataSnapshot.child("url_photo_profile")
+                        .getValue().toString()).centerCrop().fit()
+                        .into(photo_profile);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         item_my_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +74,6 @@ public class MyProfileAct extends AppCompatActivity {
             }
         });
 
-        btn_edit_profile = findViewById(R.id.btn_edit_profile);
         btn_edit_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,5 +90,10 @@ public class MyProfileAct extends AppCompatActivity {
                 startActivity(signout);
             }
         });
+    }
+
+    public void getUsernameLocal(){
+        SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+        username_key_new = sharedPreferences.getString(username_key,"");
     }
 }
